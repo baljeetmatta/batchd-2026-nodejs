0674c13070641a47e4199b6a60fa443e3671047c
Baljeet <baljeet@Baljeets-MacBook-Air.local>
2026-09-09 11:05:31 +0530

    authentication and react

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const sess=require("express-session");//function
 const path=require("path")
 const fs=require("fs");
 const app=express();
+const cors=require("cors");
+const userRoutes=require("./routes/userRoutes");
+//   /dashboard, /profile
+
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
+app.use(express.json());
+
 app.use(express.static("."));
 app.use(express.urlencoded({extended:true}))
 app.use(sess({
@@ -14,6 +24,8 @@ app.use(sess({
     }
 
 }))
+app.use("/user",auth,userRoutes);
+
 //extended:false->Basic data
 //variable=value&variable=value
 //complex 
@@ -67,6 +79,36 @@ app.post("/signup",(req,res)=>{
 })
 
 
+// app.post("/login",(req,res)=>{
+
+//     //1. Fetch users
+//     //2. Username password exists
+//     fs.readFile("./users.json","utf-8",(err,data)=>{
+//         let users=[]
+//         if(err)
+//             users=[];
+//         else
+//             users=JSON.parse(data);
+
+//         let results=users.filter((item)=>{
+//             if(item.username==req.body.username && item.password==req.body.password)
+//                 return true;
+
+//         })
+//         if(results.length==0)
+//             res.send("Invalid user/password")
+//         else
+//         {
+//             req.session.name=results[0].name;
+
+//             res.redirect("/user/dashboard")
+//         }
+        
+
+//     })
+// })
+
+
 app.post("/login",(req,res)=>{
 
     //1. Fetch users
@@ -84,28 +126,55 @@ app.post("/login",(req,res)=>{
 
         })
         if(results.length==0)
-            res.send("Invalid user/password")
+            res.json({success:false,message:"Invalid user/password"});
+
+            //res.send("Invalid user/password")
         else
         {
             req.session.name=results[0].name;
-
-            res.redirect("/dashboard")
+res.json({success:true,message:"Welcome"});
+         //   res.redirect("/user/dashboard")
         }
         
 
     })
 })
-app.get("/dashboard",(req,res)=>{
+// app.get("/dashboard",auth,(req,res)=>{
 
-    if(req.session.name)
-    res.send("Welcome to dashboard "+req.session.name);
-else
-    res.sendFile(path.join(__dirname,"./login.html"));
+//      res.send("Welcome to dashboard "+req.session.name);
+
+// //     if(req.session.name)
+// //     res.send("Welcome to dashboard "+req.session.name);
+// // else
+// //     res.redirect("/login");
+
+//     //res.sendFile(path.join(__dirname,"./login.html"));
 
 
 
+// })
+
+// app.get("/profile",auth, (req,res)=>{
+//     res.send("Profile Page")
+// })
+
+
+app.get("/login",(req,res)=>{
+
+     res.sendFile(path.join(__dirname,"./login.html"));
 })
+function auth(req,res,next)
+{
+    if(req.session.name)
+        next();
+    else
+        return res.redirect("/login");
+
+
+
+}
 app.listen(5000,(err)=>{
     if(!err)
         console.log("Server Started...")
 });
+//AUTHENTICATION
